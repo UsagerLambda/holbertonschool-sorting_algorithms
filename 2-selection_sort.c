5d1cd03e662dbcf1510e4916f107d1d901e77f61
UsagerLambda <clement.chassem@gmail.com>
2024-08-07 09:40:42 +0200

    Fix P1T2 & Fix betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,26 +2,36 @@
 /**
  * selection_sort - Sorts a array of integers in ascending order
  * using the selection sort algorithm
- * @array: Tha array to be sorted
+ * @array: The array to be sorted
  * @size: The size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_size, temp;
+size_t i, j, min_index;
+int temp;
 
-for (i = 0; i < size; i++)
+/*Décale i de 1 vers la droite à la fin de chaque traversée de j sur array*/
+for (i = 0; i < size - 1; i++)
 {
-min_size = i;
+min_index = i; /* min_index se positionne sur l'élément à comparer */
+/* Itére dans la partie non triée du tableau */
+/* pour trouver le plus petit élément du tableau */
 for (j = i + 1; j < size; j++)
 {
-	/* compare min_size avec chaque elements du tableau */
-	if (array[min_size] > array[j])
-	{
-		temp = array[min_size];
-		array[min_size] = array[j];
-		array[j] = temp;
-		print_array(array, size);
-	}
+	/* chaque fois que array[j] tombe sur un chiffre inférieur à min_index */
+	if (array[j] < array[min_index])
+	min_index = j; /* min_index devient j*/
+
+}
+/* Après avoir parcouru la partie non triée du tableau et trouvé (ou non) */
+/* un élément plus petit que array[min_index] */
+/* Effectue l'échange des éléments */
+if (min_index != i)
+{
+	temp = array[i];
+	array[i] = array[min_index];
+	array[min_index] = temp;
+	print_array(array, size);
 }
 }
 }
