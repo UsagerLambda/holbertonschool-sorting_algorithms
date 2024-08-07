c8daef358102bb68f58bf3d8f1ddef6b315b299e
dosleam <jeremy.sousa05@gmail.com>
2024-08-07 11:20:35 +0000

    fix betty & gcc P1T1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,3 +7,40 @@
  */
 
 void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	current = *list;
+	current = current->next;
+
+	while (current != NULL)
+	{
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			temp = current;
+
+			if (current->next != NULL)
+			current->next->prev = temp->prev;
+			current->prev->next = temp->next;
+			current = current->prev;
+			temp->prev = current->prev;
+			temp->next = current;
+
+			if (current->prev != NULL)
+			current->prev->next = temp;
+			current->prev = temp;
+
+			if (temp->prev == NULL)
+			*list = temp;
+
+			print_list(*list);
+			current = current->prev;
+		}
+		current = current->next;
+	}
+}
