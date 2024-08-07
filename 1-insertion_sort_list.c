8de0eff79afc01618bbe0d8f31c2a70a52db18f3
dosleam <jeremy.sousa05@gmail.com>
2024-08-07 09:35:12 +0000

    add 1 files P1T1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: pointer head to list
+ */
+
+void insertion_sort_list(listint_t **list)
