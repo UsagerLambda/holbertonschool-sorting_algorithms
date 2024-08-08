e5b25ced95fd53810ffc87f24dbd02c82a8ee4ad
dosleam <jeremy.sousa05@gmail.com>
2024-08-08 15:08:46 +0000

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -35,11 +35,15 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 			}
 		}
 	}
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-	
-	print_array(array, size);
+
+	if (i + 1 != high)
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		
+		print_array(array, size);
+	}
 	
 	/* retourne indice du pivot */
 	return (i + 1);
